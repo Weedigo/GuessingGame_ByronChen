d220feb345bfba2eeacb20f59e517f17fb983c8f
BT_1N3_04 <bchen9694@bths.edu>
2018-10-11 14:50:59 -0400

    assada

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
 
 public class GuessGame {
-    public static void GuessingGame(int difficulty) {
-        Scanner name = new.Scanner(System.in);
-        String username = name.nextLine();
+    public static void main(String[] args) {
+        //GET USERNAME
+        Scanner input = new Scanner(System.in);
+        String username = input.nextLine();
+        //MODE SELECTOR AND NUMBER GENERATOR
         System.out.print("Type 1 for numbers 1-10\n" +
                          "Type 2 for numbers 1-100\n" +
                          "Type 3 for numbers 1-1000\n" +
                          "Type 4 for numbers 1-10000\n");
-        Scanner chose = new Scanner(System.in);
-        int chosen = chose.nextInt();
-        if(chosen == 1){
-            double random = Math.random()
-            if(chosen == 2){
-                if(chosen == 3) {
-                    if(chosen == 4){
-                    } else {
-                        System.out.print("Choose the difficulty from 1-4.");
+        int chosen = input.nextInt();;
+        int random = 0;
+        if(chosen <= 4) {
+            if (chosen == 1) {
+                random = (int) (Math.random() * 11);
+                if (chosen == 2) {
+                    random = (int) (Math.random() * 101);
+                    if (chosen == 3) {
+                        random = (int) (Math.random() * 101);
+                        if (chosen == 4) {
+                            random = (int) (Math.random() * 10001);
+                        }
                     }
                 }
             }
         }
-
+        System.out.print("Guess Number");
+        int gass = input.nextInt();
+        int tries = 0;
+        while (gass != random){
+            System.out.print("Guess again.");
+            tries++;
+        }
+        if (gass == random){
+            System.out.print("Correct, it took " + tries + username);
+        }
     }
 }
